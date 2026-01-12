aac8b82005641ba7395d37d7b9f4e4f4dd468cf2
Brenno <brenno.mgonc@gmail.com>
2026-01-12 17:23:40 -0300

    Add banner redirect based on time of day

--- api/banner.ts
+++ api/banner.ts
@@ -1,1 +1,13 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const hour = new Date().getHours();
+
+  const banner =
+    hour >= 7 && hour < 19
+      ? "https://raw.githubusercontent.com/BrennoMG1/BrennoMG1/main/assets/banner_day.png"
+      : "https://raw.githubusercontent.com/BrennoMG1/BrennoMG1/main/assets/banner_night.png";
+
+  res.redirect(banner);
+}
 
